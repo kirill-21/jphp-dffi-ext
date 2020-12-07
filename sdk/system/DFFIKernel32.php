3fe52592639d13c42dc73540c259db65e2b0e6ea
Xset-s <xsetthe@gmail.com>
2020-12-07 15:38:06 +0100

    SDK;

---
+++ sdk/system/DFFIKernel32.php
@@ -0,0 +1,17 @@
+<?php
+namespace system;
+
+/**
+ *
+ * Class DFFIKernel32
+ * @package system
+ */
+class DFFIKernel32
+{
+    /**
+     * @return bool
+     */
+    public static function writeMemoryValue($hProcess, $lpBaseAddress, $value): bool
+    {
+    }
+}
